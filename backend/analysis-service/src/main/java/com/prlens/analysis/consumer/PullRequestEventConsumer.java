b23a4a683ec344eccdce401eac8ec5988d0b9e33
wangly <wangleungy77@gmail.com>
2026-06-24 18:02:04 -0700

    add producer in analysis service to publish jobs

--- backend/analysis-service/src/main/java/com/prlens/analysis/consumer/PullRequestEventConsumer.java
+++ backend/analysis-service/src/main/java/com/prlens/analysis/consumer/PullRequestEventConsumer.java
@@ -1,14 +1,22 @@
 package com.prlens.analysis.consumer;
 
+import com.prlens.analysis.service.AnalysisService;
 import org.springframework.kafka.annotation.KafkaListener;
 import  org.springframework.stereotype.Component;
-import com.prlens.analysis.model.GithubPullRequestEvent;
+import com.prlens.common.events.GitHubPullRequestEvent;
 
 @Component
 public class PullRequestEventConsumer {
 
+    private final AnalysisService service;
+
+    public PullRequestEventConsumer(AnalysisService service) {
+        this.service = service;
+    }
+
     @KafkaListener(topics = "pr-events", groupId = "analysis-service")
-    public void consume(GithubPullRequestEvent event) {
+    public void consume(GitHubPullRequestEvent event) {
         System.out.println("Received PR event: " + event);
+        service.createAnalysisJob(event);
     }
 }
