067c2675b429c31b0c2d877439a79066aacd6365
wangly <wangleungy77@gmail.com>
2026-07-13 22:44:10 -0700

    add pull request workflow

--- backend/workflow-service/src/main/java/com/prlens/workflow/service/WorkflowOrchestrator.java
+++ backend/workflow-service/src/main/java/com/prlens/workflow/service/WorkflowOrchestrator.java
@@ -1,23 +1,30 @@
 package com.prlens.workflow.service;
 
 import com.prlens.common.enums.DocJobType;
-import com.prlens.common.events.DocJobEvent;
-import com.prlens.common.events.IssueCreatedEvent;
+import com.prlens.common.events.*;
+import com.prlens.workflow.producer.CodingJobProducer;
 import com.prlens.workflow.producer.DocJobProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class WorkflowOrchestrator {
-    public final DocJobProducer producer;
+    public final DocJobProducer docJobProducer;
+    public final CodingJobProducer codingJobProducer;
 
-    public WorkflowOrchestrator(DocJobProducer docJobProducer) {
-        this.producer = docJobProducer;
+    public WorkflowOrchestrator(
+            DocJobProducer docJobProducer,
+            CodingJobProducer codingJobProducer) {
+        this.docJobProducer = docJobProducer;
+        this.codingJobProducer = codingJobProducer;
     }
 
     public void handleIssueCreated(IssueCreatedEvent event) {
         DocJobEvent job = new DocJobEvent(
+                UUID.randomUUID().toString(),
                 UUID.randomUUID().toString(),
                 event.issueId(),
                 event.title(),
@@ -25,6 +32,29 @@ public class WorkflowOrchestrator {
                 event.repository(),
                 DocJobType.DESIGN_DOC
         );
-        producer.publish(job);
+        this.docJobProducer.publish(job);
+    }
+
+    public void handleArtifactGenerated(ArtifactGeneratedEvent event) {
+        CodingJobEvent codingJobEvent = new CodingJobEvent(
+                UUID.randomUUID().toString(),
+                UUID.randomUUID().toString(),
+                event.issueId(),
+                event.repository(),
+                event.storagePath(),
+                "main"
+        );
+        log.info(
+                "Dispatching coding job. eventId={}, jobId={}, issueId={}, designDocPath={}",
+                codingJobEvent.eventId(),
+                codingJobEvent.jobId(),
+                codingJobEvent.issueId(),
+                codingJobEvent.designDocPath()
+        );
+        codingJobProducer.publish(codingJobEvent);
+    }
+
+    public void handlePullRequestEvent(PullRequestEvent event) {
+
     }
 }
